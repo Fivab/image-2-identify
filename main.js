6b3843b2265d70f72645e9255da5685946be48d5
Fivab <83701817+Fivab@users.noreply.github.com>
2021-06-09 16:20:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Webcam.set({
+  //  width:384,
+   // height:316,
+   // image_format:'png',
+    //png_quality:100,
+
+    //constraints:{
+      //  facingMode:"environment"
+    //}
+//});
+
+//camera=document.getElementById("camera");
+//Webcam.attach('#camera');
